5dff92479864ab92928c629361d9763fe977bd06
ArpanBatra01 <arpanbatra60@gmail,com>
2024-12-30 16:48:59 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: '#001529', padding: '20px', textAlign: 'center', color: '#fff' }}>
+      <h1>Movie Search & Watchlist</h1>
+    </header>
+  );
+};
+
+export default Header;
